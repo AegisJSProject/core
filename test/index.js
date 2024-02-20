37084ce902f8a59ef3d4ce830be19b17d5aa8d65
Chris Zuber <shgysk8zer0@gmail.com>
2024-02-20 09:50:14 -0800

    Add TrustedTypes module

--- test/index.js
+++ test/index.js
@@ -1,8 +1,18 @@
-import { html, css, replaceStyles, getUniqueSelector } from '@shgysk8zer0/aegis';
+import { html, css, replaceStyles, getUniqueSelector, createPolicy } from '@shgysk8zer0/aegis';
 import { reset } from '@shgysk8zer0/aegis-styles/reset.js';
 import { baseTheme, lightTheme, darkTheme } from '@shgysk8zer0/aegis-styles/theme.js';
 import { getComponent } from '@shgysk8zer0/aegis-component';
 import './dad-joke.js';
+
+createPolicy('default', {
+	createHTML: (input, ...args) => {
+		const el = document.createElement(input);
+		el.setHTML(input);
+		console.log({ input, args });
+		return el.innerHTML;
+	},
+	createScript: () => trustedTypes.emptyScript,
+});
 
 const scope = getUniqueSelector();
 
